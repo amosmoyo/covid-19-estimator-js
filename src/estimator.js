97e71085765aa607fe5203bbc79c71097de6df24
amosmoyo <amosmoyo5300@gmail.com>
2020-04-10 17:04:34 +0300

    new changes

--- src/estimator.js
+++ src/estimator.js
@@ -2,14 +2,14 @@ const days = (periodType, timeToElapse) => {
   let day;
   const time = timeToElapse;
   switch (periodType) {
-      case 'months':
-        day = time * 30;
-      break;
-      case 'weeks':
-        day = time * 7;
-      break;
-      default:
-        day = time;   
+    case 'months':
+      day = time * 30;
+    break;
+    case 'weeks':
+      day = time * 7;
+    break;
+    default:
+      day = time;   
   }
 
   const ans = 2**(Math.floor((day / 3)));
