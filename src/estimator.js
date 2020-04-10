1c1c803f7adad2e61a11feabe69a5d214ff2a36c
amosmoyo <amosmoyo5300@gmail.com>
2020-04-10 13:50:02 +0300

    new changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,62 @@
-const covid19ImpactEstimator = (data) => data;
+const days = (periodType,timeToElapse) => {
+  let day;
+  let time = timeToElapse;
+  switch(periodType) {
+    case 'months':
+      day = time * 30;
+    break;
+    case 'weeks':
+      day = time * 7;
+    break;
+    default:
+      day = time   
+  }
+  let ans = Math.pow(2, Math.floor((day/3)));
+  return ans;
+};
+
+
+
+const impact = {};
+const severeImpact = {};
+
+const estimator = (val) => {
+  let input = val;
+  let reportedCases = input.reportedCases;
+  let time = input.timeToElapse;
+  let period = input.periodType;
+  let  beds = input.totalHospitalBeds;
+  
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * days(period,time);
+  impact.severeCasesByRequestedTime = Math.floor(0.15 * impact.infectionsByRequestedTime);
+  impact.hospitalBedsByRequestedTime = hospitalBeds(impact.severeCasesByRequestedTime, beds);
+  impact.casesForICUByRequestedTime = Math.floor(0.05 * impact.infectionsByRequestedTime);
+  
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * days(period, time);
+  severeImpact.severeCasesByRequestedTime = Math.floor(0.15 * severeImpact.infectionsByRequestedTime);
+  severeImpact.hospitalBedsByRequestedTime = hospitalBeds(severeImpact.severeCasesByRequestedTime, beds);
+  severeImpact.casesForICUByRequestedTime = Math.floor(0.05 * severeImpact.infectionsByRequestedTime);
+
+}
+
+const hospitalBeds = (severe, beds) => {
+   let occupiedBeds = Math.floor(0.65 * beds);
+   let accualCapacity = Math.floor(0.925 * beds );
+   let remainBedActualCapity = Math.floor(accualCapacity - occupiedBeds);
+   let requiredBeds = severe - remainBedActualCapity;
+   return requiredBeds;
+}
+
+const covid19ImpactEstimator = (data) => {
+    estimator(data);
+
+    return {
+        data,
+        impact,
+        severeImpact,
+    }
+}
 
 export default covid19ImpactEstimator;
