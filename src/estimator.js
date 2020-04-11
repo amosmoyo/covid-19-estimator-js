07130a8a3b16233c4008559d82d2ab884934eea8
amosmoyo <amosmoyo5300@gmail.com>
2020-04-11 20:16:42 +0300

    new changes

--- src/estimator.js
+++ src/estimator.js
@@ -21,7 +21,7 @@ function days (periodType, timeToElapse){
   return ans;
 }
 
-function hospitalBeds (severe, beds){
+function hospitalBeds(severe, beds){
   const occupiedBeds = Math.floor(0.65 * beds);
 
   const accualCapacity = Math.floor(0.925 * beds);
@@ -37,13 +37,13 @@ const incomeLost = (infected, time, income) => ((infected * time * income).toFix
 const estimator = (val) => {
   impact.currentlyInfected = val.reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * days(
-
-  val.periodType, val.timeToElapse);
+  val.periodType, val.timeToElapse
+  );
   impact.severeCasesByRequestedTime = Math.floor(0.15 * impact.infectionsByRequestedTime);
 
   impact.hospitalBedsByRequestedTime = hospitalBeds(
-
   impact.severeCasesByRequestedTime, val.totalHospitalBeds
+
   );
 
   impact.casesForICUByRequestedTime = Math.floor(0.05 * impact.infectionsByRequestedTime);
@@ -57,16 +57,18 @@ const estimator = (val) => {
 
   severeImpact.currentlyInfected = val.reportedCases * 50;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * days(
+  val.periodType, val.timeToElapse
 
-  val.periodType, val.timeToElapse);
-  severeImpact.severeCasesByRequestedTime = Math.floor(0.15 * severeImpact.infectionsByRequestedTime);
+  );
+  severeImpact.severeCasesByRequestedTime = Math.floor(
+  0.15 * severeImpact.infectionsByRequestedTime);
 
   severeImpact.hospitalBedsByRequestedTime = hospitalBeds(
-
-  severeImpact.severeCasesByRequestedTime, val.totalHospitalBeds);
-  severeImpact.casesForICUByRequestedTime = Math.floor(0.05 * severeImpact.infectionsByRequestedTime);
+  severeImpact.severeCasesByRequestedTime, val.totalHospitalBeds
+  );
+  severeImpact.casesForICUByRequestedTime = Math.floor(
+  0.05 * severeImpact.infectionsByRequestedTime);
   severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
-
   0.02 * severeImpact.infectionsByRequestedTime);
   severeImpact.dollarsInFlight = incomeLost(
     severeImpact.infectionsByRequestedTime,
